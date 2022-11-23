272d6bbe9bbc3fa590fc3be18689db3bb0f957b9
ezeiyoke cosmas <43332174+coxxy666@users.noreply.github.com>
2022-11-23 09:39:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+const mark_height = 1.69
+const mark_weight = 78
+
+const mark_bmi = (78) / (1.69 * 1.69)
+
+console.log(mark_bmi)
+
+const john_height = 1.95
+const john_weight = 92 
+
+const john_bmi = (92) / (1.95 * 1.95)
+
+console.log(john_bmi)
+
+
+console.log(john_bmi > mark_bmi)
